a83d22ad3cddba2e83455bc7dd5e3a5039a6aed6
Stefan <stefan.irk96@mail.ru>
2021-06-08 11:28:28 +0800

    add new files, finished header section

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,23 @@
-$( function () {
+// $( function () {
 
 
-})
+// })
+
+$(document).ready(function () {
+  $('.menu__wrapper').click(function () {
+    if (!$('.menu__wrapper').hasClass('openDone')) {
+      $('.menu__wrapper').addClass('openDone');
+      $('nav').css("right", "0px");
+    } else {
+      $('.menu__wrapper').removeClass('openDone');
+      $('nav').css("right", "-310px");
+    }
+  });
+
+  $(window).resize(function () {
+    if ($(window).width() > 1250) {
+      $('nav ul li').removeAttr('style');
+    }
+  });
+});
+
